63a61787d4abf88b70713365f97247ca00428faa
HeikkiTakalo <heikkitakalo@windowslive.com>
2024-10-09 09:23:50 +0300

    Added inputs and options to calcform

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./header"
+import Calcform from "./calcform"
 
 function App() {
   return (
     <div>
       <Header />
+      <Calcform />
     </div>
   )
 }
+
 
 export default App
---
+++ src/calcform.jsx
@@ -0,0 +1,13 @@
+function Calcform() {
+
+
+
+    return (
+        <><input></input><p></p>
+            <select>
+                <option value="plus">+</option>
+            </select><p></p>
+            <input></input></>
+    )
+}
+export default Calcform
