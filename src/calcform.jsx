c4745cab87ddc9b7f973fa6c71cc67f1e020974f
HeikkiTakalo <heikkitakalo@windowslive.com>
2024-10-09 10:05:50 +0300

    Added functionality to calculator

--- src/calcform.jsx
+++ src/calcform.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react"
+
 function Calcform() {
 
+    const [firstNumber, setFirstNumber] = useState('');
+    const [secondNumber, setSecondNumber] = useState('');
+    const [result, setResult] = useState(null);
+    const calculate = () => {
+        setResult(parseFloat(firstNumber) + parseFloat(secondNumber));
+    }
 
 
     return (
-        <><input></input><p></p>
+        <>
+            <input type="number" value={firstNumber} onChange={(e) => setFirstNumber(e.target.value)}></input>
+            <p></p>
             <select>
                 <option value="plus">+</option>
-            </select><p></p>
-            <input></input></>
+            </select>
+            <p></p>
+            <input type="number" value={secondNumber} onChange={(e) => setSecondNumber(e.target.value)}></input>
+            <p></p>
+            <button onClick={calculate}>Calculate</button>
+            <p>Sum of numbers:{firstNumber} and {secondNumber} is {result}</p>
+        </>
+
     )
 }
 export default Calcform
