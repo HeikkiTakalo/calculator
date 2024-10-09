ec4525dcc8b3871e9a16817908344e60a519de87
HeikkiTakalo <heikkitakalo@windowslive.com>
2024-10-09 08:48:32 +0300

    Header component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Header from "./header"
 
 function App() {
-  
   return (
     <div>
-
+      <Header />
     </div>
   )
 }
---
+++ src/header.jsx
@@ -0,0 +1,6 @@
+function Header() {
+    return (
+        <h1>Calculator</h1>
+    )
+}
+export default Header
